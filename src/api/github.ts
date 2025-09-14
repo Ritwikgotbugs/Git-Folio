3fd6b80871a50446e9f30b76baf5d500ae883464
Ritwikgotbugs <shritwik04@gmail.com>
2025-09-15 01:43:10 +0530

    fix: configures local vars

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,4 +1,4 @@
-const WEB_URL = process.env.REACT_APP_BACKEND_URL || "https://gitfolio.ddns.net";
+const WEB_URL = "http://localhost:4000";
 
 export async function fetchGitHubUser(username: string) {
   const response = await fetch(`${WEB_URL}/api/github/user/${username}`);
